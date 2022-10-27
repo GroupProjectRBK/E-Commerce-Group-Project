25709aed9b0a3cfe38f2469fa335ef07bff1885c
mazen kouki <mazenlouki@gmail.com>
2022-10-27 22:27:56 +0100

    front routes

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -2,6 +2,8 @@ const express= require("express")
 const router = express.Router()
 const {getAll}= require("../controllers/products")
 const { register ,getUser,login,logout} = require("../controllers/users")
+const {insertP,updateP,deleteP,updateUser,deleteUser,getAllP}=require("../controllers/userdashboard")
+const {buy,getAllbuys}=require('../controllers/buys')
 
 // products routes
 router.get("/getAll" , getAll)
@@ -12,4 +14,13 @@ router.post('/get-user',getUser)
 router.post('/logout',logout)
 //usersdashboard routes
 router.post("/postd")
+router.post("/postP",insertP)
+router.put("/modifie",updateP)
+router.delete('/delete',deleteP)
+router.put("/Uuser",updateUser)
+router.delete('/deleteuser',deleteUser)
+router.post('/getallp',getAllP)
+//Buys routes
+router.post('/buy',buy)
+router.post('/allbuys',getAllbuys)
 module.exports = router
